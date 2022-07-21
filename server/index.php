2cb9ce6b124bddd5c39927137c386a6e61f994e0
Chase Patterson <chase.patterson@sourcetoad.com>
2022-07-21 15:44:58 -0400

    Add prettier message

--- server/index.php
+++ server/index.php
@@ -5,23 +5,34 @@ require __DIR__ . '/vendor/autoload.php';
 use GuzzleHttp\Client;
 
 $client = new Client();
-$res = $client->request('POST', 'https://hooks.slack.com/services/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', [
+$res = $client->request('POST', 'https =>//hooks.slack.com/services/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', [
     'body' => json_encode([
         'text' => 'Yo fridge open',
-        'blocks' => [
+        'attachments' => [
             [
-                'type' => 'section',
-                'text' => [
-                    'type' => 'mrkdwn',
-                    'text' => 'Someone _please_ close the fridge!',
-                ],
-                'accessory' => [
-                    'type' => 'image',
-                    'image_url' => 'https://i.pinimg.com/originals/64/bf/3c/64bf3c8cf34b05c222882ffc52a14088.jpg',
-                    'alt_text' => 'open fridge door',
+                'text' => 'Choose a game to play',
+                'fallback' => 'You are unable to choose a game',
+                'callback_id' => 'wopr_game',
+                'color' => '#3AA3E3',
+                'attachment_type' => 'default',
+                'actions' => [
+                    [
+                        'name' => 'game',
+                        'text' => "I'll get it!",
+                        'style' => 'danger',
+                        'type' => 'button',
+                        'value' => 'close',
+                        'confirm' => [
+                            'title' => 'Are you sure?',
+                            'text' => 'Are you actually going to close the door?',
+                            'ok_text' => 'Yes',
+                            'dismiss_text' => 'No',
+                        ],
+                    ],
                 ],
             ],
         ],
+        'image_url' => 'https =>//is5-ssl.mzstatic.com/image/thumb/Purple3/v4/d3/72/5c/d3725c8f-c642-5d69-1904-aa36e4297885/source/256x256bb.jp =>//i.pinimg.com/originals/64/bf/3c/64bf3c8cf34b05c222882ffc52a14088.jpg',
     ]),
     'headers' => ['Content-Type' => 'application/json'],
 ]);
@@ -31,4 +42,3 @@ if ($res->getStatusCode() === 200) {
 } else {
     echo 'failure';
 }
-
