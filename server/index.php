f1a0dc253e4b24b864a339d1d03c7121911bdf99
Chase Patterson <chase.patterson@sourcetoad.com>
2022-07-21 15:48:06 -0400

    Fix request url

--- server/index.php
+++ server/index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 use GuzzleHttp\Client;
 
 $client = new Client();
-$res = $client->request('POST', 'https =>//hooks.slack.com/services/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', [
+$res = $client->request('POST', 'https://hooks.slack.com/services/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', [
     'body' => json_encode([
         'text' => 'Yo fridge open',
         'attachments' => [
