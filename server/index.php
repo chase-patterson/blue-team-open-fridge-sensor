9c2af9ee6fdf25235af5ca9ac237d7d09098253d
Chase Patterson <chase.patterson@sourcetoad.com>
2022-07-21 16:33:35 -0400

    Add guzzle exception handling

--- server/index.php
+++ server/index.php
@@ -4,18 +4,18 @@ require __DIR__ . '/vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
-$client = new Client();
-$res = $client->request('POST', 'https://hooks.slack.com/services/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', [
-    'body' => json_encode([
-        'text' => $_GET['status'] === 'closed'
-            ? 'The fridge is closed ―Purple People-Eaters'
-            : 'The fridge is open ―Purple People-Eaters',
-    ]),
-    'headers' => ['Content-Type' => 'application/json'],
-]);
+try {
+    $client = new Client();
+    $res = $client->request('POST', 'https://hooks.slack.com/services/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', [
+        'body' => json_encode([
+            'text' => $_GET['status'] === 'closed'
+                ? 'The fridge is closed ―Purple People-Eaters'
+                : 'The fridge is open ―Purple People-Eaters',
+        ]),
+        'headers' => ['Content-Type' => 'application/json'],
+    ]);
 
-if ($res->getStatusCode() === 200) {
     echo 'success';
-} else {
+} catch (Exception $e) {
     echo 'failure';
 }
