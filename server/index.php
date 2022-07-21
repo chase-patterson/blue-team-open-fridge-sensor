d9e6c0261dd74f78ab5f0d89109da31ae740058e
Chase Patterson <chase.patterson@sourcetoad.com>
2022-07-21 16:32:01 -0400

    Add query string parsing

--- server/index.php
+++ server/index.php
@@ -7,7 +7,9 @@ use GuzzleHttp\Client;
 $client = new Client();
 $res = $client->request('POST', 'https://hooks.slack.com/services/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', [
     'body' => json_encode([
-        'text' => 'Test message',
+        'text' => $_GET['status'] === 'closed'
+            ? 'The fridge is closed ―Purple People-Eaters'
+            : 'The fridge is open ―Purple People-Eaters',
     ]),
     'headers' => ['Content-Type' => 'application/json'],
 ]);
